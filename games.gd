35530756211a841a45e3eb9ff47d216e93ee21b1
David <37672966+Ieida@users.noreply.github.com>
2024-04-05 14:31:32 +0200

    bunch of improvements

--- games.gd
+++ games.gd
@@ -1,23 +1,31 @@
 extends Node
 
-const directory: String = ""
-var games: Dictionary
+var games: Array
 
 func _enter_tree():
 	load_games()
 
 func load_games():
 	### WARNING: OS.get_executable_path() isn't reliable on macOS
-	var path = directory if directory != "" else OS.get_executable_path().get_base_dir()
+	### Get path to launcher executable
+	var path = OS.get_executable_path().get_base_dir()
+	### Load all of the games
 	var dirs = DirAccess.get_directories_at(path)
 	for d in dirs:
-		var fs = DirAccess.get_files_at("/".join([path, d]))
-		var exe: String
-		var info: String
-		for f in fs:
+		var full_path = "/".join([path, d])
+		var file_paths = DirAccess.get_files_at(full_path)
+		### Load game
+		var info = {}
+		for f in file_paths:
+			var file_path = "/".join([full_path, f])
 			if f.get_extension() == "exe":
-				exe = "/".join([path, d, f])
+				info[&"exe_path"] = file_path
 			elif f.get_extension() == "json":
-				info = "/".join([path, d, f])
-		var s = FileAccess.get_file_as_string(info)
-		games[exe] = JSON.parse_string(s)
+				var s = FileAccess.get_file_as_string(file_path)
+				var p = JSON.parse_string(s)
+				if not p: printerr("Failed parsing %s" % file_path)
+				else:
+					info[&"info_path"] = file_path
+					info.merge(p)
+		### Save loaded game
+		games.append(info)
